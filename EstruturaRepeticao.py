ba9d2a35f1500692abd51cfba2a86355ef6ddf8e
Jeziel Oliveira <jeziel.oliveira.paula@gmail.com>
2026-05-27 06:57:47 -0300

    Adicionando o While

--- EstruturaRepeticao.py
+++ EstruturaRepeticao.py
@@ -4,4 +4,14 @@ listaNomes = ["João", "Maria", "Pedro", "Ana", "Carlos"]
 #Estrutura de repetição para cumprimentar cada pessoa da lista
 for nome in listaNomes:
     print(f"Olá, {nome}!\nBem-vindo(a) ao curso de Python!")
+
+
+
+indice = 0 # Começamos na posição 0 (João)
+
+# Enquanto o índice for menor que o tamanho da lista (5)
+while indice < len(listaNomes):
+    nome = listaNomes[indice]
+    print(f"Olá, {nome}!\nBem-vindo(a) ao curso de Python!!!!!!")
     
+    indice += 1 # Adicionamos 1 ao índice para ir para o próximo nome na próxima rodada
